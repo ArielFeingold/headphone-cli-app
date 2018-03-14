ce4f0ffef51f250524fed52be0b97947402db282
Ariel Feingold <arielfeingold@Andriels-MBP.fios-router.home>
2018-03-14 16:54:55 -0400

    trying to revert to older version

---
+++ lib/headphones.rb
@@ -0,0 +1,9 @@
+require 'open-uri'
+require 'nokogiri'
+# require 'pry'
+require 'colorize'
+
+
+require_relative './headphones/cli'
+require_relative './headphones/scraper'
+require_relative "./headphones/version"
