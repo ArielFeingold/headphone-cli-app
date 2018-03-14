ebfc6626da60a15ddb78a8c38af3ad0675b925d9
Ariel Feingold <arielfeingold@Andriels-MBP.fios-router.home>
2018-03-14 17:07:17 -0400

    project name changed

---
+++ lib/headphones-buyers-guide-cli-gem.rb
@@ -0,0 +1,8 @@
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+require 'colorize'
+
+require_relative "./headphones-buyers-guide-cli-gem/version"
+require_relative './headphones-buyers-guide-cli-gem/cli'
+require_relative './headphones-buyers-guide-cli-gem/scraper'
